d972d77eb3af13cccaef6902ed06d3de217a85fd
DmitryKersh <d.karpukhin@mail.ru>
2021-06-18 21:45:44 +0300

    add: fxml file for main window made FXMLLoader work

--- src/main/java/com/github/dmitrykersh/bugs/gui/MainWindow.java
+++ src/main/java/com/github/dmitrykersh/bugs/gui/MainWindow.java
@@ -2,19 +2,26 @@ package com.github.dmitrykersh.bugs.gui;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 
 public class MainWindow extends Application {
     @Override
     public void start(final Stage stage) throws Exception {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainWindow.class.getResource("/gameWindow.fxml"));
+            Scene scene = loader.load();
+            stage.setScene(scene);
+
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args){
